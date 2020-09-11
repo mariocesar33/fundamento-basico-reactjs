78cf54bf3fa1c80f8a4fe8cc5e5ba6b5d698c944
Mário César Silva <mcgato33@hotmail.com>
2020-09-11 12:26:13 -0100

    fundamento

---
+++ babel-webpack/fundamento-react/src/App.js
@@ -0,0 +1,14 @@
+import React from 'react';
+
+import Header from './components/Header';
+
+function App() {
+  return (
+    <>
+      <Header />
+      <h2>vamos descobrir uma nova forma de programar!</h2>
+    </>
+  );
+}
+
+export default App;
